b65094dec9fb267e3d42f58bca349e04d314f9ef
SDCLxD <123998607+SDCLxD@users.noreply.github.com>
2023-10-09 16:01:25 -0300

    Update server.js

--- server.js
+++ server.js
@@ -90,5 +90,5 @@ app.post('/rc/snd', (req, res) => {
     });
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 7493;
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
