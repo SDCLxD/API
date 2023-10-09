d36b93d276569e0b8be787fe22717f3a1d0ddd01
SDCLxD <123998607+SDCLxD@users.noreply.github.com>
2023-10-09 15:38:16 -0300

    Update server.js

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const app = express();
 app.use(bodyParser.json());
 
 const db = mysql.createConnection({
-  host: '34.228.105.30',
+  host: '168.138.157.178',
   user: 'saidvm',
   password: 'senha123',
-  database: 'wlsaid'
+  database: 'wlsistem'
 });
 
 db.connect((error) => {
@@ -17,17 +17,20 @@ db.connect((error) => {
   console.log('Conectado ao servidor MySQL.');
 });
 
-app.get('/get', function(req, res) {
-  res.send("Olá");
-});
-
-app.post('/script/whitelist', (req, res) => {
-  const { chave1, hwide } = req.body;
+app.post('api/auth/whitelist/script.lua', (req, res) => {
+  const { chave1, hwide, rng } = req.body;
   const { chave } = req.query;
 
-  if (!chave1 || !hwide) {
-    return res.status(400).json({ message: 'Chave ou HWID não fornecido' });
+  const rng_value = rng;
+  const modifiedRng = (rng_value - 6 + 5 * 4 / 3) % 2;
+  const url = 
+
+  if (!chave1 || !hwide || !rng) {
+    return res.status(400).json({ Error: 'Chave ou HWID não fornecido, resposta inválida.' });
   }
+
+  console.log('Valor RNG recebido:', rng);
+  console.log('Valor RNG modificado:', modifiedRng);
 
   const query = 'SELECT * FROM whitelist WHERE chave = ?';
   db.query(query, [chave], (error, results) => {
@@ -41,33 +44,20 @@ app.post('/script/whitelist', (req, res) => {
           if (updateError) throw updateError;
           console.log('HWID atualizado para:', hwide);
         });
-      } else if (whitelistEntry.hwid === hwide) {
-        res.status(200).json({ message: 'Whitelist realizada com sucesso' });
+      } else if (whitelistEntry.hwid === hwide || modifiedRng === 1.6666666666860692) {
+        res.status(200).json({ message: 'Whitelist realizada com sucesso', rng: modifiedRng, url: script });
       } else {
         res.status(403).json({ message: 'Chave ou HWID inválidos.', error: '[Verify] HWID does not match key, ask for an HWID reset' });
       }
     }
   });
-});
-
-app.post('/api/auth', (req, res) => {
-  const { rng } = req.body;
-  const rng_value = rng;
-  const modifiedRng = (rng_value - 6 + 5 * 4 / 3) % 2;
-  
-  if (!rng) {
-    return res.status(400).json({ message: 'RNG ausente' });
-  }
-  
-  console.log('Valor RNG recebido:', rng);
-  console.log('Valor RNG modificado:', modifiedRng);
 
   if (modifiedRng === 1.6666666666860692 ) {
-      res.status(200).json({ rng: modifiedRng });
-    } else {
-      res.status(403).json({ error: 'Someone tried to crack, or just a whitelist error.' });
-    }
-  });
+    res.status(200).json({ rng: modifiedRng });
+  } else {
+    res.status(403).json({ Error: 'Someone tried to crack, or just a whitelist error.' });
+  }
+});
 
 app.post('/rc/snd', (req, res) => {
   const { key, hwid, i } = req.body;
@@ -100,5 +90,5 @@ app.post('/rc/snd', (req, res) => {
     });
 });
 
-const PORT = process.env.PORT || 7492;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
