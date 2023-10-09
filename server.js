53cfb9f353e666f77bbcb4012be24600cfdc049b
SDCLxD <123998607+SDCLxD@users.noreply.github.com>
2023-10-09 20:11:33 -0300

    Update server.js

--- server.js
+++ server.js
@@ -21,20 +21,13 @@ app.get('/get', function(req, res) {
   res.send("Olá");
 });
 
-app.post('api/auth/whitelist/script.lua', (req, res) => {
-  const { chave1, hwide, rng } = req.body;
+app.post('/script/whitelist', (req, res) => {
+  const { chave1, hwide } = req.body;
   const { chave } = req.query;
 
-  const rng_value = rng;
-  const modifiedRng = (rng_value - 6 + 5 * 4 / 3) % 2;
-  const url = "https://raw.githubusercontent.com/SDCLxD/API/master/lua";
-
-  if (!chave1 || !hwide || !rng) {
-    return res.status(400).json({ Error: 'Chave ou HWID não fornecido, resposta inválida.' });
+  if (!chave1 || !hwide) {
+    return res.status(400).json({ message: 'Chave ou HWID não fornecido' });
   }
-
-  console.log('Valor RNG recebido:', rng);
-  console.log('Valor RNG modificado:', modifiedRng);
 
   const query = 'SELECT * FROM whitelist WHERE chave = ?';
   db.query(query, [chave], (error, results) => {
@@ -48,98 +41,18 @@ app.post('api/auth/whitelist/script.lua', (req, res) => {
           if (updateError) throw updateError;
           console.log('HWID atualizado para:', hwide);
         });
-      } else if (whitelistEntry.hwid === hwide || modifiedRng === 1.6666666666860692) {
-        res.status(200).json({ message: 'Whitelist realizada com sucesso', rng: modifiedRng});
+      } else if (whitelistEntry.hwid === hwide) {
+        res.status(200).json({ message: 'Whitelist realizada com sucesso' });
         const luaScript = `
-          getgenv().fastfarm = true
-
-          function findneck()
-          	local old = game.Players.LocalPlayer.CameraMinZoomDistance
-          	local old2 = game.Players.LocalPlayer.CameraMaxZoomDistance
+          function fastfarm()
               local data = game.ReplicatedStorage["Stats"..game.Players.LocalPlayer.Name]
-          
+        
               if data.Stats.Peli.Value < 300 then
-                  local vt = 500
-                  local pl = game.Players.LocalPlayer.Character.HumanoidRootPart
-                  local CFrameEnd = CFrame.new(1009.09015, 8.99998474, 1179.3689)
-                  local info = TweenInfo.new((game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart").Position).magnitude / vt,Enum.EasingStyle.Linear)
-                  local tween = game:GetService("TweenService"):Create(pl, info, {CFrame = CFrameEnd})
-                  tween:Play()
-                  tween.Completed:Wait()
-          
-              if tween.Completed then
-                  task.wait(2)
-                  local args = {
-                      [1] = {
-                          [1] = "takequest",
-                          [2] = "Find Sarah's necklace"
-                      }
-                  }
-          
-                  game:GetService("ReplicatedStorage").Events.Quest:InvokeServer(unpack(args))
-                  
-                  game.Players.LocalPlayer.CameraMinZoomDistance = 128
-          		game.Players.LocalPlayer.CameraMaxZoomDistance = 256
-          		game.Players.LocalPlayer.CameraMinZoomDistance = 256
-                  end
-              end
-          
-          	function GetNeak()
-              if Workspace.Effects:FindFirstChild("Folder") then
-          		for i, v in ipairs(Workspace.Effects.Folder:GetDescendants()) do
-          			if v.Name == "Part" and v.Parent.Name == "Folder" and v:FindFirstChild("Mesh") and v.Mesh.TextureId == "http://www.roblox.com/asset/?id=28461501" and v:FindFirstChild("ClickDetector") then
-          				return v
-          			end
-          		end
-          	end
-          	end
-          
-              for _, child in pairs(game.Players.LocalPlayer.Character:GetDescendants()) do
-                  if child:IsA("BasePart") and child.CanCollide == true then
-                      child.CanCollide = false
-                  end
-              end
-                     
-          	repeat wait()
-              local cac = GetNeak()
-          	if cac then 
-          		local rac = cac.CFrame + Vector3.new(0,5,0)
-          		local vt = 500
-                  local pl = game.Players.LocalPlayer.Character.HumanoidRootPart
-                  local CFrameEnd = CFrame.new(rac.X, rac.Y, rac.Z)
-                  local info = TweenInfo.new((game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart").Position).magnitude / vt,Enum.EasingStyle.Linear)
-                  local tween = game:GetService("TweenService"):Create(pl, info, {CFrame = CFrameEnd})
-                  tween:Play()
-                  tween.Completed:Wait()
-                  wait(1)
-          		if cac:FindFirstChild("ClickDetector") then 
-          			fireclickdetector(cac.ClickDetector,2)
-          		end
-          	end
-          	
-          	until data.Quest.CurrentQuest.Value=="None" or data.Quest.QuestProgress.Value==1
-          	local vt = 250
-          	local pl = game.Players.LocalPlayer.Character.HumanoidRootPart
-          	local CFrameEnd = CFrame.new(1009.09015, 8.99998474, 1179.3689)
-              local info = TweenInfo.new((game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart").Position).magnitude / vt,Enum.EasingStyle.Linear)
-              local tween = game:GetService("TweenService"):Create(pl, info, {CFrame = CFrameEnd})
-              tween:Play()
-          	tween.Completed:Wait()
-          	if tween.Completed then
-                  local args = {
-                      [1] = {
-                          [1] = "returnitem"
-                      }
-                  }
-          
-                  game:GetService("ReplicatedStorage").Events.Quest:InvokeServer(unpack(args))
-                  game.Players.LocalPlayer.Character.HumanoidRootPart.CanCollide = true
-                  game.Players.LocalPlayer.CameraMaxZoomDistance = old2
-                  game.Players.LocalPlayer.CameraMinZoomDistance = old
+                  print("menos grana")
               end
           end
           
-          findneck()
+          fastfarm()
         `;
         res.set('Content-Type', 'text/plain');
         res.send(luaScript);
@@ -148,13 +61,26 @@ app.post('api/auth/whitelist/script.lua', (req, res) => {
       }
     }
   });
+});
+
+app.post('/api/auth', (req, res) => {
+  const { rng } = req.body;
+  const rng_value = rng;
+  const modifiedRng = (rng_value - 6 + 5 * 4 / 3) % 2;
+  
+  if (!rng) {
+    return res.status(400).json({ message: 'RNG ausente' });
+  }
+  
+  console.log('Valor RNG recebido:', rng);
+  console.log('Valor RNG modificado:', modifiedRng);
 
   if (modifiedRng === 1.6666666666860692 ) {
-    res.status(200).json({ rng: modifiedRng });
-  } else {
-    res.status(403).json({ Error: 'Someone tried to crack, or just a whitelist error.' });
-  }
-});
+      res.status(200).json({ rng: modifiedRng });
+    } else {
+      res.status(403).json({ error: 'Someone tried to crack, or just a whitelist error.' });
+    }
+  });
 
 app.post('/rc/snd', (req, res) => {
   const { key, hwid, i } = req.body;
@@ -187,5 +113,5 @@ app.post('/rc/snd', (req, res) => {
     });
 });
 
-const PORT = process.env.PORT || 7493;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
